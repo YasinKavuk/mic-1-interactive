67a8cb9c9c5a8c0ee314c226cf48a3a2cd22853c
Max Brockmann <max@tbrockmann.de>
2023-10-06 16:54:13 +0200

    closes Tutor mode file selection #166

--- src/app/View/tutor-mode/tutor-mode.component.ts
+++ src/app/View/tutor-mode/tutor-mode.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tutor-mode',
   templateUrl: './tutor-mode.component.html',
-  styleUrls: ['./tutor-mode.component.css']
+  styleUrls: ['./tutor-mode.component.scss']
 })
 export class TutorModeComponent implements OnInit {
+  files:File[] = [];
+  hideDropzone = false;
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
+
+  
+  onSelect(event: any){
+    this.files.push(...event.addedFiles)
+    console.log(this.files)
+    console.log("onSelect")
+  }
+
+  onRemove(event: any){
+    this.files.splice(this.files.indexOf(event),1)
+    console.log("onRemove")
+  }
+
+  onAddFiles(){
+    console.log("onAddFiles")
+    this.hideDropzone = true;
+    //this.controller.importFiles(this.files);
+    //this.dialogRef.close();
+  }
+
+
+
+
 
 }
