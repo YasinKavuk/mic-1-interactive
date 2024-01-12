a13271cae676f25c60fa0c8c505331ea6245d739
YasinKavuk <yasinkavuk100@gmail.com>
2024-01-12 20:19:07 +0100

    #190, #191: tokens now include the line in editor and the token types are more specific and not grouped to much already.

--- src/app/Model/macro-tokenizer.service.ts
+++ src/app/Model/macro-tokenizer.service.ts
@@ -1,7 +1,46 @@
 import { Injectable } from '@angular/core';
 import { MacroProviderService } from './macro-provider.service';
-import { Token } from './micro-tokenizer.service';
 import { BehaviorSubject } from 'rxjs';
+
+
+const spec: any = [
+  // New Label: e.g Label1:
+  [/^.*:/ , "NEW_LABEL"],
+
+  // Comment
+  [/^\/\/.*/, "COMMENT"],
+
+  // Operation code, identifiers and numbers
+  [/^[A-Z]+(_[A-Z]+)*/, "OPCODE"],
+  [/^(0x[a-fA-F0-9]+)/, "HEXNUMBER"],
+  [/^((-)?\d+)/, "NUMBER"],
+  [/^([a-z]([a-zA-Z0-9]+))/, "IDENTIFIER"],
+
+  //Fields
+  [/^.constant/, "FIELD_CONST"],
+  [/^.main/, "FIELD_MAIN"],
+  [/^.var/, "FIELD_VAR"],
+  [/^.method [a-zA-Z]([a-zA-Z0-9]+)?\(([a-z]([a-zA-Z0-9]+)?(, )?)*\)/, "FIELD_METH"],
+
+  //End Fields
+  [/^.end-constant/, "FIELDEND_CONST"],
+  [/^.end-main/, "FIELDEND_MAIN"],
+  [/^.end-var/, "FIELDEND_VAR"],
+  [/^.end-method/, "FIELDEND_METH"],
+
+  //Constant & Variable
+  [/^[a-z]([a-zA-Z0-9]+)? (-)?\d+/, "NEW_CONSTANT"],
+  [/^[a-z]([a-zA-Z0-9]+)?/, "NEW_VARIABLE"],
+
+  [/^\n/, "BREAK"],
+  [/^\s/, "SPACE"]
+];
+
+export interface Token{
+  type: string;
+  value: string;
+  line: number;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,46 +48,14 @@ import { BehaviorSubject } from 'rxjs';
 
 
 export class MacroTokenizerService {
-  Spec: any= [
-    // New Label: e.g Label1:
-    [/^.*:/ , "NEW_LABEL"],
-
-    // Whitespace
-    [/^\s+/, null],
-
-    // Comment
-    [/^\/\/.*/, null],
-
-    // Numbers:
-    [/^0x[a-fA-F0-9]+/, "Hex_Number"],
-    [/^\d+/, "NUMBER"],
-
-    // Mnemonics
-    [/^([A-Z]+(_[A-Z]+)*(( 0x[a-fA-F0-9]+)|( (-)?\d+)|( [a-zA-Z0-9]+)|( [a-z]([a-zA-Z0-9]+))|( [a-zA-Z]([a-zA-Z0-9]+)))*)/, "MNEMONIC"],
-
-    //Fields
-    [/^.constant/, "FIELD"],
-    [/^.main/, "FIELD"],
-    [/^.var/, "FIELD"],
-    [/^.method [a-zA-Z]([a-zA-Z0-9]+)?\(([a-z]([a-zA-Z0-9]+)?(, )?)*\)/, "FIELD"],
-
-    //End Fields
-    [/^.end-constant/, "FIELDEND"],
-    [/^.end-main/, "FIELDEND"],
-    [/^.end-var/, "FIELDEND"],
-    [/^.end-method/, "FIELDEND"],
-
-    //Constant & Variable
-    [/^[a-z]([a-zA-Z0-9]+)? (-)?\d+/, "NEW_CONSTANT"],
-    [/^[a-z]([a-zA-Z0-9]+)?/, "NEW_VARIABLE"],
-  ];
-
-
+  
   private string: string = "";
   private curser: number = 0;
 
   private token: Token = null;
   private tokens: Token[] = [];
+
+  private line: number = 1;
 
   private _errorFlasher = new BehaviorSubject({ line: 0, error: "" });
   public errorFlasher$ = this._errorFlasher.asObservable();
@@ -67,10 +74,13 @@ export class MacroTokenizerService {
       if(this.token == null){
         break;
       }
-      console.log(this.token);
+      // console.log(this.token);
       this.tokens.push(this.token);
     }
     this.resetTokenizer();
+    console.log("---------------------------")
+    console.table(this.tokens)
+    console.log("---------------------------")
   }
 
   // initialized the tokenizer and also tokenizes. It isn't using the program in the editors, it uses macrocode that is passed to this method
@@ -109,7 +119,7 @@ export class MacroTokenizerService {
 
     const string = this.string.slice(this.curser);
 
-    for (const [regexp, tokenType] of this.Spec){
+    for (const [regexp, tokenType] of spec){
       const tokenValue = this.match(regexp, string);
 
       // could not match this rule, try the other ones
@@ -121,10 +131,18 @@ export class MacroTokenizerService {
       if (tokenType == null) {
         return this.getNextToken();
       }
+      else if(tokenType == "BREAK"){
+        this.line++;
+        return this.getNextToken();
+      }
+      else if(tokenType == "SPACE"){
+        return this.getNextToken();
+      }
 
       return{
         type: tokenType,
         value: tokenValue,
+        line: this.line,
       }
     }
 
@@ -134,6 +152,7 @@ export class MacroTokenizerService {
   resetTokenizer(){
     this.string = "";
     this.curser = 0;
+    this.line = 1;
   }
 
   getTokens(){
