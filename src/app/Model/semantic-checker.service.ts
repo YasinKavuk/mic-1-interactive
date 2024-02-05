9c76084402828100643055dc1c19b6b18403c032
Max Brockmann <max@tbrockmann.de>
2024-02-05 22:54:14 +0100

    Added basic Code and placeholder Functions for  Method validation; #193

--- src/app/Model/semantic-checker.service.ts
+++ src/app/Model/semantic-checker.service.ts
@@ -17,12 +17,12 @@ export class SemanticCheckerService {
     console.log(JSON.stringify(ast, (key, value) => (key === 'parent' ? undefined : value), 2))
     this.validOpcodes = Object.keys(opcodes);
     this.ast = ast;
-    this.hasMainMethod();
+    this.checkForMainMethod();
     this.checkConstants();
-    console.log("keine Fehler!");
+    this.checkMethods();
   }
 
-  private hasMainMethod() {
+  private checkForMainMethod() {
     const methodNode = this.ast.children[2]
 
     if (methodNode.children.length === 0) {
@@ -69,6 +69,62 @@ export class SemanticCheckerService {
     const matched = /^([a-zA-Z]([a-zA-Z0-9]*))/.exec(node.value);
     console.log(matched)
     return matched !== null;
+  }
+
+  private checkMethods() {
+    const allMethods = this.ast.children[2].children;
+
+    for (let method of allMethods) {
+      this.checkMethod(method);
+    }
+  }
+
+  private checkMethod(methodNode: ASTNode) {
+
+    const [opCodes, variables, labels, methodParameters] = methodNode.children;
+
+    for (let line of opCodes.children) {
+      this.checkIfValidMethodLine(line);
+    }
+
+    this.checkMethodVariables(variables);
+    this.checkMethodParameters(methodParameters);
+    this.checkMethodLabels(labels);
+
+  }
+
+  private checkIfValidMethodLine(line: ASTNode) {
+    console.log(JSON.stringify(line, (key, value) => (key === 'parent' ? undefined : value), 2))
+    /* TODO: 
+      1. Line muss mit identifier anfangen, Identifier muss in Opcode liste sein
+      2. Identifier muss in Opcode Liste sein
+      3. Parameter müssen Zahlen sein
+      4. Parameter müssen in ein Byte passen
+    */
+  }
+
+  private checkMethodVariables(variables: ASTNode) {
+    console.log(JSON.stringify(variables, (key, value) => (key === 'parent' ? undefined : value), 2))
+    /* TODO: 
+      1. Identifier muss ein String sein
+      2. Identifier darf nicht mit Konstantennamen Kollidieren
+      3. Nach einem Identifier darf nichts weiteres kommen
+    */
+  }
+
+  private checkMethodParameters(parameters: ASTNode) {
+    /* TODO: 
+      1. Parameter müssen ein String sein
+      2. Parametername muss wohlgeformt sein
+      3. Parametername darf nicht mit Konstantennammen Kollidieren 
+    */
+  }
+
+  private checkMethodLabels(labels: ASTNode){
+    /* TODO: 
+      1. Label müssen der Namenskonvention entsprechen
+      2. Label dürfen (innherhalb einer Methode) nicht mehrmals vorkommen
+    */
   }
 
 
